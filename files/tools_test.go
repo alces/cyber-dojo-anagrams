32e23bea9a0000436ef0a4b212adb934b848482f
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-23 08:11:59 +0000

    7 ran tests, predicted red, got red

--- files/tools_test.go
+++ files/tools_test.go
@@ -8,3 +8,13 @@ import (
 func TestChopLetterEmptyString(t *testing.T) {
     assert.Nil(t, chopLetter(""))
 }
+
+func TestChopLetterOneChar(t *testing.T) {
+    expected := []chopped{
+        {
+            head: "a",
+            tail: "",
+        },
+    }
+    assert.Equal(t, expected, chopLetter("a"))
+}
