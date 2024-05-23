8a847fabd62a9f7230a7c9010a43edd7b3a88a29
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-23 07:44:25 +0000

    3 ran tests, predicted green, got red

--- files/anagrams.go
+++ files/anagrams.go
@@ -1,5 +1,13 @@
 package anagrams
 
-func anagrams(word string) []string {
-    return []string{}
+func anagrams(word string) (result []string) {
+    if len(word) == 0 {
+        return
+    }
+    
+    if len(word) == 1 {
+        return []string{word}
+    }
+    
+    return
 }
