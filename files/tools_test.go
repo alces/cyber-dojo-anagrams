4cfa66abef4cfc8196f8ad090433bd146667fa06
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-23 08:39:26 +0000

    17 ran tests, predicted green, got green

--- files/tools_test.go
+++ files/tools_test.go
@@ -39,3 +39,27 @@ func TestChopLetterTwoChars(t *testing.T) {
         assert.Contains(t, actual, e)
     }
 }
+
+func TestChopLetterThreeChars(t *testing.T) {
+        expected := []chopped{
+        {
+            head: "a",
+            tail: "bc",
+        },
+        {
+            head: "b",
+            tail: "ac",
+        },
+        {
+            head: "c",
+            tail: "ab",
+        },
+    }
+    
+    actual := chopLetter("abc")
+    assert.Len(t, actual, 3)
+    
+    for _, e := range expected {
+        assert.Contains(t, actual, e)
+    }
+}
