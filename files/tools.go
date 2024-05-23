87ea1b707b5b3036b5eed641cca15f7c8701ade1
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-23 08:36:53 +0000

    15 ran tests, predicted amber, got red

--- files/tools.go
+++ files/tools.go
@@ -11,12 +11,14 @@ func chopLetter(word string) (result []chopped) {
     }
     
     if len(word) == 1 {
-        return []chopped{
-            {
-                head: word,
-            },
-        }
+        return []chopped{{head: word}}
     }
+    
+    result = append(result, chopped{head: string(word[0]), tail: word[1:]})
+    
+    for i := 1; i < len(word); i++ {
+        result = append(result, chopped{head: string(word[0]), tail: word[:i] + word[i+1:]})
+    }    
 
     return
 }
