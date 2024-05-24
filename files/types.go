d4725955588bb8997c527deb52ea71c0dca72b73
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-24 13:38:34 +0000

    56 ran tests, predicted red, got red

--- files/types.go
+++ files/types.go
@@ -10,4 +10,8 @@ type stringSet map[string]struct{}
 func (ss stringSet) add(word string) {
     var value struct{}
     ss[word] = value
-}    
+}
+
+func (ss stringSet) keys() []string {
+    return []string{}
+}
--- files/types_test.go
+++ files/types_test.go
@@ -12,3 +12,12 @@ func TestStringSetAdd(t *testing.T) {
     assert.Len(t, ss, 1)
     assert.Contains(t, ss, "abc")
 }
+
+func TestStringSetKeys(t *testing.T) {
+    ss := stringSet{}
+    ss.add("abc")
+    ss.add("bcd")
+    
+    keys := ss.keys()
+    assert.Contains(t, keys, "abc")
+}
