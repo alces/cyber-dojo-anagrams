0bbcf408cbef894ca5edbe418e620eb019a7edde
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-24 07:00:57 +0000

    49 ran tests, predicted green, got green

--- files/anagrams.go
+++ files/anagrams.go
@@ -6,10 +6,6 @@ type chopped struct {
 }
 
 func anagrams(word string) (result []string) {
-    if len(word) == 0 {
-        return
-    }
-    
     if len(word) == 1 {
         return []string{word}
     }
