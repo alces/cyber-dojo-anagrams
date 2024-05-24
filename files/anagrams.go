88f0f89f2c25f11031a93d3e8ee01dcf5c991ab8
Hgkdw5 <Hgkdw5@cyber-dojo.org>
2024-05-24 07:01:13 +0000

    50 ran tests, predicted green, got red

--- files/anagrams.go
+++ files/anagrams.go
@@ -6,10 +6,6 @@ type chopped struct {
 }
 
 func anagrams(word string) (result []string) {
-    if len(word) == 1 {
-        return []string{word}
-    }
-    
     for _, c := range chopLetter(word) {
         for _, t := range(anagrams(c.tail)) {
             result = append(result, c.head + t)
